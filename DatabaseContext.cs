057cb19a7ca13ce9d84be98100b602beb96eef3d
hayesch19 <chrishayespro@gmail.com>
2019-09-18 10:18:02 -0400

    Updated Database

--- DatabaseContext.cs
+++ DatabaseContext.cs
@@ -29,7 +29,7 @@ namespace explosiveapi
       {
         var envConn = Environment.GetEnvironmentVariable("DATABASE_URL");
         // #warning Be sure to update to your correct connection string to the point to the correct database
-        var conn = "server=localhost;database=SdgTemplate";
+        var conn = "server=localhost;database=ExplosiveDatabase";
         if (envConn != null)
         {
           conn = ConvertPostConnectionToConnectionString(envConn);
